8456a3caac8a61babd8394b1652e87bd8ef60d1a
steve wirts <stevewirts@gmail.com>
2015-03-12 13:57:49 -0400

    seed gh-pages

---
+++ components/fin-hypergrid/polymer/js/features/fin-hypergrid-feature-thumbwheel-scrolling.js
@@ -0,0 +1,22 @@
+(function() {
+
+    'use strict';
+
+    Polymer({ /* jshint ignore:line */
+        handleWheelMoved: function(grid, e) {
+            var primEvent = e.primitiveEvent;
+            var deltaY = primEvent.wheelDeltaY || -primEvent.deltaY;
+            var deltaX = primEvent.wheelDeltaX || -primEvent.deltaX;
+            if (deltaY > 0) {
+                grid.scrollBy(0, -1);
+            } else if (deltaY < -0) {
+                grid.scrollBy(0, 1);
+            } else if (deltaX > 0) {
+                grid.scrollBy(-1, 0);
+            } else if (deltaX < -0) {
+                grid.scrollBy(1, 0);
+            }
+        }
+    });
+
+})(); /* jshint ignore:line */
